1b4a370c26e1d8d7e0596a28ce07065b6f0799ac
Jake Woods <jake@jakewoods.net>
2021-12-11 13:27:21 +1100

    Flake support
    `cargo run` doesn't work yet under wayland, but it builds!

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+
+        rust = (pkgs.rust-bin.nightly.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer-preview"
+          ];
+        });
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            rust
+            pkgs.pkgconfig
+          ];
+
+          buildInputs = [
+            pkgs.xorg.libX11
+            pkgs.xlibs.libX11
+            pkgs.wayland
+          ];
+
+  # APPEND_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+  #   libGL
+  #   vulkan-loader
+  #   xorg.libX11
+  #   xlibs.libXcursor
+  #   xlibs.libXi
+  #   xlibs.libXrandr
+  # ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.xlibs.libX11
+            pkgs.xlibs.libXcursor
+            pkgs.xlibs.libXrandr
+            pkgs.xlibs.libXi
+            pkgs.libxkbcommon
+            pkgs.wayland
+            pkgs.xwayland
+          ];
+        };
+      }
+    );
+}
