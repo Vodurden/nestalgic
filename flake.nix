46cc82becbea7da4aad5af05dc069d9508d68b67
Jake Woods <jake@jakewoods.net>
2022-05-21 17:35:05 +1000

    Use nixGL for vulkan detection in `nix develop`
    This lets us use vulkan on nixos even if the host nixpkgs doesn't align
    with nestalgic's nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,18 +1,19 @@
 {
-  description = "A very basic flake";
+  description = "A toy NES emulator";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    nixgl.url = "github:guibou/nixGL/";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, nixgl }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [(import rust-overlay)];
+          overlays = [(import rust-overlay) nixgl.overlay];
         };
 
         rust = (pkgs.rust-bin.nightly.latest.default.override {
@@ -32,6 +33,7 @@
             pkgs.xorg.libX11
             pkgs.xlibs.libX11
             pkgs.wayland
+            pkgs.nixgl.nixVulkanIntel
           ];
 
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
@@ -44,6 +46,12 @@
             pkgs.wayland
             pkgs.xwayland
           ];
+
+          shellHook = ''
+            export VK_LAYER_PATH=$(nixVulkanIntel printenv VK_LAYER_PATH)
+            export VK_ICD_FILENAMES=$(nixVulkanIntel printenv VK_ICD_FILENAMES)
+            export LD_LIBRARY_PATH=$(nixVulkanIntel printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH
+          '';
         };
       }
     );
